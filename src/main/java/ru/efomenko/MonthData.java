44bab2230294a994e35828f1ba70c1028033cde2
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2023-02-24 00:51:05 +0500

    Добавил и реализовал в классе MonthData методы bestSeries(), maxSteps(), sumStepsFromMonth()

--- src/main/java/ru/efomenko/MonthData.java
+++ src/main/java/ru/efomenko/MonthData.java
@@ -3,4 +3,38 @@ package ru.efomenko;
 public class MonthData {
     int[] days = new int[31];
 
+    void printDaysAndStepsFromMonth() {
+        for (int j = 0; j < days.length; j++) {
+            System.out.println((j + 1) + "день: " + days[j]);
+        }
+    }
+
+    int sumStepsFromMonth() {
+        int sumSteps = 0;
+        for (int day : days) {
+            sumSteps += day;
+        }
+        return sumSteps;
+    }
+
+    int maxSteps() {
+        int maxStepsDay = 0;
+        for (int day : days) {
+            if (maxStepsDay < day) {
+                maxStepsDay = day;
+            }
+        }
+        return maxStepsDay;
+    }
+
+    int bestSeries(int goalByStepsPerDay) {
+        int bestSeries = 0;
+        for (int day : days) {
+            if (day >= goalByStepsPerDay) {
+                bestSeries++;
+            }
+        }
+        return bestSeries;
+    }
+
 }
