65b9e00ac43a4be49be4b3305efbde5bd664987f
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2023-02-24 00:04:07 +0500

    Применил авто форматтирование

--- src/main/java/ru/efomenko/StepTracker.java
+++ src/main/java/ru/efomenko/StepTracker.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 public class StepTracker {
     private final static MonthData[] monthData = new MonthData[12];
     private final Scanner scanner;
-    public StepTracker(Scanner scanner){
+
+    public StepTracker(Scanner scanner) {
         this.scanner = scanner;
-        for (int j = 0; j< monthData.length;j++){
+        for (int j = 0; j < monthData.length; j++) {
             monthData[j] = new MonthData();
         }
     }
-    public void addNewNumberStepsPerDay(){
+
+    public void addNewNumberStepsPerDay() {
         System.out.println("Введите номер месяца");
         int month = scanner.nextInt();
         System.out.println("Введите номер дня");
         int day = scanner.nextInt();
         System.out.println("Введите количество шагов");
         int steps = scanner.nextInt();
-        if(month > 31 || month < 1 || day > 31 || day < 1 || steps < 0){
+        if (month > 31 || month < 1 || day > 31 || day < 1 || steps < 0) {
             System.out.println("Введены не верные параметры");
             return;
         }
-        monthData[month-1].days[day-1]=steps;
+        monthData[month - 1].days[day - 1] = steps;
 
     }
 }
