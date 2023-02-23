0c42e82a4f34bd4f213ccd9c69f5fa0ce7e78658
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2023-02-23 23:47:23 +0500

    Добавил в класс StepTracker приватные поля Scanner и monthData и добавил конструктор c аргументом Scanner и инициализировал массив monthData;

--- src/main/java/ru/efomenko/StepTracker.java
+++ src/main/java/ru/efomenko/StepTracker.java
@@ -1,5 +1,14 @@
 package ru.efomenko;
 
-public class StepTracker {
+import java.util.Scanner;
 
+public class StepTracker {
+    private final MonthData[] monthData = new MonthData[12];
+    private Scanner scanner;
+    public StepTracker(Scanner scanner){
+        this.scanner = scanner;
+        for (int j = 0; j< monthData.length;j++){
+            monthData[j] = new MonthData();
+        }
+    }
 }
